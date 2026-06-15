a4d899e6b3f5ce4cbc5e82be9ea429422e67a61a
Yash-Sharma-Programer <yashbca029@gmail.com>
2026-06-15 21:33:57 +0530

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,41 @@
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom"
-import { CartProvider } from './context/CartContext'
-import { AuthProvider } from './context/AuthContext'
-import Navbar from './components/Navbar'
-import Header from './components/Header'
-import Product from './components/Product'
-import SignIn from './Pages/SigIn'
-import LogIn from './Pages/LogIn'
-import AdminLogin from './Pages/AdminLogin'
-import AdminDashboard from './Pages/AdminDashboard'
-import AddProduct from './Pages/AddProduct'
-import Profile from './Pages/Profile'
+
+// Ek Layout component jo baki pages ke upar Navbar dikhayega
+const LayoutWithNavbar = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet /> {/* Baaki ke pages yahan render honge */}
+    </>
+  )
+}
 
 function App() {
   return (
-    <>
     <AuthProvider>
       <CartProvider>
         <BrowserRouter>
           <Routes>
-            {/* Admin routes - no Navbar */}
+            {/* 1. Admin Routes - Inme Navbar nahi dikhega */}
             <Route path="/adminlogin" element={<AdminLogin />} />
             <Route path="/admin" element={<AdminDashboard />} />
 
-            {/* Main app routes - with Navbar */}
-            <Route path="/*" element={
-              <>
-                <Navbar />
-                <Routes>
-                  <Route path="/signin" element={<SignIn />} />
-                  <Route path="/login" element={<LogIn />} />
-                  <Route path="/addproduct" element={<AddProduct />} />
-                  <Route path="/profile" element={<Profile />} />
-                  <Route path="/" element={
-                    <>
-                      <Header />
-                      <Product />
-                    </>
-                  } />
-                </Routes>
-              </>
-            } />
+            {/* 2. Main Routes - In sabhi pages par Navbar sath mein dikhega */}
+            <Route element={<LayoutWithNavbar />}>
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/login" element={<LogIn />} />
+              <Route path="/addproduct" element={<AddProduct />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/" element={
+                <>
+                  <Header />
+                  <Product />
+                </>
+              } />
+            </Route>
           </Routes>
         </BrowserRouter>
       </CartProvider>
     </AuthProvider>
-            </>
   )
 }
-
-export default App
