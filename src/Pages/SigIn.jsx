2fabf22a3b32a497e9825caf5335e2e956abfba9
Yash-Sharma-Programer <yashbca029@gmail.com>
2026-06-15 15:57:35 +0530

    commit

--- src/Pages/SigIn.jsx
+++ src/Pages/SigIn.jsx
@@ -20,7 +20,7 @@ const SignIn = () => {
         setError("")
 
         try {
-            const res = await fetch("https://ecom-backend-six-sigma.vercel.app/signin", {
+            const res = await fetch("https://ecom-backend-ovxs.vercel.app/signin", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 credentials: "include",
